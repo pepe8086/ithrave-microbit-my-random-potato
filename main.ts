aa8fc49039b7d02464812d01a5c0a8b1e69d370b
pepe8086 <pepe8086@users.noreply.github.com>
2022-01-21 11:47:04 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let counter = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    counter = randint(0, 100)
+    while (counter >= 0) {
+        basic.pause(200)
+        counter += -1
+        basic.showNumber(counter)
+        if (input.isGesture(Gesture.ThreeG)) {
+            counter = 0
+        }
+    }
+    music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(500)
+    basic.showIcon(IconNames.No)
+})
+basic.forever(function () {
+	
+})
